cda91e882f08913362172ce7a47a53790b953998
swapnil <who.swap@gmail.com>
2026-04-29 16:52:08 +0000

    Code challenge submission for question: 442 Find All Duplicates in an Array

---
+++ 442 Find All Duplicates in an Array/442find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+            if(nums.empty()) return {};
+    vector<int> ans;
+    for(int i=0;i<nums.size();i++){
+        int idx = abs(nums[i])-1;
+        if(nums[idx]<0){
+        ans.push_back(abs(nums[i]));
+        }
+        
+            nums[idx] = -nums[idx];
+        
+    }
+    return ans;
+
+    }
+};
