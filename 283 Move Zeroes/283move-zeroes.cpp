5519476b8576c50efe784c3af62f772be18a50c8
swapnil <who.swap@gmail.com>
2026-05-23 05:58:05 +0000

    Code challenge submission for question: 283 Move Zeroes

---
+++ 283 Move Zeroes/283move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left=0;int right =0;
+        while(right<nums.size()){
+            if(nums[right]!=0){
+                swap(nums[right],nums[left]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
